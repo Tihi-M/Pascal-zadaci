6fab189ef419870ca128d0465be19d3cf4d32dde
Tihi-M <148490644+Tihi-M@users.noreply.github.com>
2023-11-25 00:51:21 +0100

    .

--- Stampanje piramida/piramida2.pas
+++ Stampanje piramida/piramida2.pas
@@ -12,33 +12,50 @@
 program piramida2;
 var visinaP:integer;
 procedure stampajPiramidu(height : integer);
-var i,j:integer;
+var i,j,k:integer;
 begin
     i:=1;
-    j:=1;
+    j:=0;
+    k:=1;
     while ( i <= height) do
       begin
-        while( j <= i) do
+        while( j < height) do
         begin
-          write(j,' ');
+          write(' ');
           j:=j+1;
           end;
-        writeLn();
-        j:=1;
+        while(k >= 1)do
+        begin
+          write(k);
+          k := k-1;   
+        end;
+        writeln();
+        j:=i;
+        k:=i+1;
         i:=i+1;
       end;
-      i:=height-1;
-       while (i > 0) do
+
+      
+    i:=height-1;
+    j:=0;
+    k:=height-1;
+    while ( i > 0) do
+      begin
+        while( j <= height-i) do
         begin
-        while (j<= i) do
-          begin
-          write(j,' ');
+          write(' ');
           j:=j+1;
           end;
-        WriteLn();
-        j:=1;
+        while(k >= 1)do
+        begin
+          write(k);
+          k := k-1;   
+        end;
+        writeln();
+        j:=0;
+        k:=i-1;
         i:=i-1;
-        end;  
+      end;
 end;
 
 begin
